e82e22c639828659bc273a59e362aacdcb3dd885
unknown <trieshakenneth.24cs@licet.ac.in>
2024-11-05 12:44:23 +0530

    added changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello my name is Triesha Kenneth - UPDATED VERSION'));
+app.get('/', (req, res) => res.send('Hello my name is Triesha Kenneth - UPDATED VERSION- hellooo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
